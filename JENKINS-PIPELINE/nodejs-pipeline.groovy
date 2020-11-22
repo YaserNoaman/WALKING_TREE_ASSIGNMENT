17d83d7037d013962551f77fef62483c21b7a1d9
YaserNoaman <yasenoaman9032@gmail.com>
2020-11-22 14:45:46 +0530

    Jenkins-Pipeline

---
+++ JENKINS-PIPELINE/nodejs-pipeline.groovy
@@ -0,0 +1,44 @@
+
+
+pipeline {
+  environment {
+    registry = "yasernoaman/docker-image-nodejs"
+    registryCredential = 'xxxxx'
+    dockerImage = ''
+  }
+  agent any
+  tools {nodejs "node" }
+  stages {
+    stage('Cloning Git') {
+      steps {
+        git 'https://github.com/YaserNoaman/noaman-code-nodejs-repo.git'
+      }
+    }
+    stage('Build') {
+       steps {
+         sh 'npm install'
+       }
+    }
+    stage('Test') {
+      steps {
+        sh 'npm test'
+      }
+    }
+    stage('Building image') {
+      steps{
+        script {
+          dockerImage = docker.build registry + ":$BUILD_NUMBER"
+        }
+      }
+    }
+    stage('Deploy Image') {
+      steps{
+         script {
+            docker.withRegistry( '', registryCredential ) {
+            dockerImage.push()
+          }
+        }
+      }
+    }
+  }
+}
